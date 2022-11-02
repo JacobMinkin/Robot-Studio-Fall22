613a99004e3762a7048c534852f28ac6e0117d79
jakemanthebakeman <53197939+jakemanthebakeman@users.noreply.github.com>
2022-11-02 14:32:35 -0400

    Update Walking-Code-v.01.py

--- Walking-Code-v.01.py
+++ Walking-Code-v.01.py
@@ -4,7 +4,7 @@ import time
 import math
 import numpy as np
 
-LX16A.initialize('/dev/ttyUSB4')
+LX16A.initialize('/dev/ttyUSB0')
 def starting_position ():
     for index in range(0, 100):
         servo1.move(120)
