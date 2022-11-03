69069063af0b31c64b1f8724757ecf1f0bbc429b
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-11-03 12:04:47 -0400

    update

--- move_motor.py
+++ move_motor.py
@@ -31,22 +31,26 @@ try:
 except ServoTimeoutError as e:
     
     quit()
-servo_pos11 = servo11.get_physical_angle()
-servo_pos12 = servo12.get_physical_angle()
-servo_pos13 = servo13.get_physical_angle()
-servo_pos14 = servo14.get_physical_angle()
-
-servo_pos21 = servo21.get_physical_angle()
-servo_pos22 = servo22.get_physical_angle()
-servo_pos23 = servo23.get_physical_angle()
-servo_pos24 = servo24.get_physical_angle()
 
 servo_front_pos = [servo11.get_physical_angle(), servo12.get_physical_angle(), servo13.get_physical_angle(), servo14.get_physical_angle()]
+servo_back_pos = [servo21.get_physical_angle(), servo22.get_physical_angle(), servo23.get_physical_angle(), servo24.get_physical_angle()]
 
-print("Servo 11: " + str(servo_pos11) + " Servo 12: " + str(servo_pos12))
-print("Servo 13: " + str(servo_pos13) + " Servo 14: " + str(servo_pos14))
+servo11_new = 209.52 
+servo12_new = 124.56
+servo13_new = 25.68 
+servo14_new = 105.84
+servo21_new = 209.52 
+servo22_new = 137.76
+servo23_new = 32.4 
+servo24_new = 109.44
 
-print("Servo 21: " + str(servo_pos21) + " Servo 22: " + str(servo_pos22))
-print("Servo 23: " + str(servo_pos23) + " Servo 24: " + str(servo_pos24))
+servo11.move (servo11_new )
+servo12.move ( servo12_new )
+servo13.move (servo13_new )
+servo14.move ( servo14_new )
+servo21.move (servo21_new )
+servo22.move ( servo22_new )
+servo23.move (servo23_new )
+servo24.move ( servo24_new )
 
 quit()
