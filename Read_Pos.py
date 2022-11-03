9e22d46442571f1af8c1b1a4070afe172d58b020
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-11-03 11:43:02 -0400

    Update Read_Pos.py

--- Read_Pos.py
+++ Read_Pos.py
@@ -33,6 +33,18 @@ except ServoTimeoutError as e:
     quit()
 servo_pos11 = servo11.get_physical_angle()
 servo_pos12 = servo12.get_physical_angle()
+servo_pos13 = servo13.get_physical_angle()
+servo_pos14 = servo14.get_physical_angle()
+
+servo_pos21 = servo21.get_physical_angle()
+servo_pos22 = servo22.get_physical_angle()
+servo_pos23 = servo23.get_physical_angle()
+servo_pos24 = servo24.get_physical_angle()
 
 print("Servo 11: " + str(servo_pos11) + " Servo 12: " + str(servo_pos12))
+print("Servo 13: " + str(servo_pos13) + " Servo 14: " + str(servo_pos14))
+
+print("Servo 21: " + str(servo_pos21) + " Servo 22: " + str(servo_pos22))
+print("Servo 23: " + str(servo_pos23) + " Servo 24: " + str(servo_pos24))
+
 quit()
