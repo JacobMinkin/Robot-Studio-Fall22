07bf41234e3c516aaca785728845948278183c1b
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-11-03 11:39:35 -0400

    Update Read_Pos.py

--- Read_Pos.py
+++ Read_Pos.py
@@ -12,14 +12,14 @@ LX16A.initialize('/dev/ttyUSB0')
     
 
 try:
-    servo11 = LX16A(13)
-    servo12 = LX16A(14)
-    #servo13 = LX16A(13)
-    #servo14 = LX16A(14)
-    #servo21 = LX16A(21)
-    #servo22 = LX16A(22)
-    #servo23 = LX16A(23)
-    #servo24 = LX16A(24)
+    servo11 = LX16A(11)
+    servo12 = LX16A(12)
+    servo13 = LX16A(13)
+    servo14 = LX16A(14)
+    servo21 = LX16A(21)
+    servo22 = LX16A(22)
+    servo23 = LX16A(23)
+    servo24 = LX16A(24)
     #servo11.set_angle_limits(0, 240)
     #servo12.set_angle_limits(0, 240)
     #servo13.set_angle_limits(0, 240)
@@ -31,6 +31,8 @@ try:
 except ServoTimeoutError as e:
     
     quit()
-servo_pos = servo12.get_physical_angle()
-print(servo_pos)
+servo_pos11 = servo11.get_physical_angle()
+servo_pos12 = servo12.get_physical_angle()
+
+print("Servo 11: " + servo_pos11 + " Servo 12: " + str(servo12.get_physical_angle()))
 quit()
