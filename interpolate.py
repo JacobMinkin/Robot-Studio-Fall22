e60d9a4bb15c9fcc612f9dcfc12034d5f4c1b1c5
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-11-14 22:55:44 -0500

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -1,10 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy import interpolate
 
 y =  np.genfromtxt('pos.csv', delimiter=',')
 size = y.shape
 x = np.arange(0, size[0])
+print(size)
 print(np.array2string(y, separator=","))
 print(np.array2string(x, separator=","))
-f = interpolate.interp1d(x, y)
+f_11 = interpolate.interp1d(x, y[:,0])
+f_12 = interpolate.interp1d(x, y[:,1])
+f_13 = interpolate.interp1d(x, y[:,2])
+f_14 = interpolate.interp1d(x, y[:,3])
 
+f_21 = interpolate.interp1d(x, y[:,4])
+f_22 = interpolate.interp1d(x, y[:,5])
+f_23 = interpolate.interp1d(x, y[:,6])
+f_24 = interpolate.interp1d(x, y[:,7])
+
+xnew = np.arange(0, 1, 0.1)
+ynew = f_11(xnew)   # use interpolation function returned by `interp1d`
+plt.plot(x, y, 'o', xnew, ynew, '-')
+plt.show()
