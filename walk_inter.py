96ad00373748191f7d612a5a48faba59e135288c
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-11-15 13:00:12 -0500

    Update walk_inter.py

--- walk_inter.py
+++ walk_inter.py
@@ -45,18 +45,18 @@ except ServoTimeoutError as e:
     
     quit()
 for z in range (0, 5):
-    for x in range (0, 500, 1):
-        if(x<250):
-            y = x+250
+    for x in range (0, 250, 1):
+        if(x<125):
+            y = x+125
         else:
-            y = x -250
-        servo11.move (f_11(x/50.0) )
-        servo12.move ( f_12(x/50.0) )
-        servo13.move (f_13(y/50.0))
-        servo14.move ( f_14(y/50.0) )
-        servo21.move (f_11(y/50.0) )
-        servo22.move ( f_12(y/50.0) )
-        servo23.move (f_13(x/50.0))
-        servo24.move ( f_14(x/50.0)) 
+            y = x -125
+        servo11.move (f_11(x/25.0) )
+        servo12.move ( f_12(x/25.0) )
+        servo13.move (f_13(y/25.0))
+        servo14.move ( f_14(y/25.0) )
+        servo21.move (f_11(y/25.0) )
+        servo22.move ( f_12(y/25.0) )
+        servo23.move (f_13(x/25.0))
+        servo24.move ( f_14(x/25.0)) 
         #time.sleep(.0001)
 quit()
