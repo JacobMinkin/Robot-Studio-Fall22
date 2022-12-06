1ac8b35287637ed03d8ef4bb0914833ad70b8efc
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-12-06 13:46:00 -0500

    Update walk_inter.py

--- walk_inter.py
+++ walk_inter.py
@@ -5,8 +5,14 @@ import time
 
 while True:
     walk_test = input("Enter time or steps: ")
+    if(walk_test == "exit"):
+        quit()
     steps = input("Enter number of steps: ")
+    if(steps == "exit"):
+        quit()
     speed = input("Enter speed: ")
+    if(speed == "exit"):
+        quit()
     speed = int(speed)
     steps = int (steps)
     if(walk_test == "steps"):
