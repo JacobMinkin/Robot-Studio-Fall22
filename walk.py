0dd6b3a7738882bc897e0fadd73247b6a9d76cdc
jakemanthebakeman <Jakemanthebakeman@gmail.com>
2022-12-07 00:27:15 -0500

    Update walk.py

---
+++ walk.py
@@ -0,0 +1,27 @@
+from lx16a import *
+from moving_functions import *
+
+
+while True:
+    speed = input("Enter speed: ")
+    if(speed == "exit"):
+        quit()
+    walk_test = input("Enter time, steps or turn: ")
+    if(walk_test == "steps"):
+        steps = input("Enter number of steps: ")
+        speed = int(speed)
+        steps = int (steps)
+        move_step(steps= steps, speed= speed)
+    elif (walk_test == "time"):
+        time_num = input("Enter time in seconds: ")
+        speed    = int(speed)
+        time_num = int (time_num)
+        move_time( endtime = time_num, speed= speed)
+    elif(walk_test == "turn"):
+        side  = input("Enter direction of turn (right or left): ")
+        steps = input("Enter number of steps: ")
+        speed = int(speed)
+        steps = int (steps)
+        move_turn(steps= steps, speed= speed, side= side)
+    else:
+        exit()
